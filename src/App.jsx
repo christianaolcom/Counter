070171beb4fb8754f1888324c06d90806c709a70
Christiana Miracle <christianamiracle686@gmail.com>
2025-03-10 15:03:15 +0100

    Controlled Counter

---
+++ src/App.jsx
@@ -0,0 +1,62 @@
+import { useState } from 'react';
+import Navbar from './Components/Navbar'
+import './App.css'
+
+
+
+function App() {
+  // let stylee = {color: '#foo', fontWeight:900}
+  // let name = 'Joseph'
+  // console.log(name);
+
+  // const time = new Date().toLocaleTimeString
+  // console.log(time);
+  
+  const [num, setnum] = useState(0)
+  const [stop, setstop] = useState('')
+  const Increment = () =>{
+    if (num >= 10) {
+      setstop('Number cannot exceed beyond 10')
+      setTimeout(() => {
+        setstop(false)
+      }, [3000]);
+    }
+    else{
+      setnum(num + 1)
+    }
+  }
+
+  
+  const Decrement = () => {
+    if (num >= 100){
+      setTimeout(() => {
+      }, [3000]);
+    }
+    else {
+      setnum(num +1)
+    }
+  }
+
+
+  return (
+    <>
+    
+        {/* <p style={stylee}>Hello World!</p>
+        <Navbar/>
+        <h1>Welcome, {name}, the time is {time}</h1> */}
+        <p className='fs-1 text-center'>Controlled Counter</p>
+        <p className='text-center fs-2 text danger m-5'>{num}</p>
+        <p>{stop}</p>
+        <button onClick={Increment} className='btn mx-2 btn-success w-25 fw-bold'> Increment +</button>
+        <button onClick={Decrement} className='btn mx-2 btn-danger w-25 fw-bold'> Decrement -</button>
+       
+    </>
+  )
+}
+
+
+
+export default App
+
+// const Increment = () => {
+//   setnum(num+1)
---
+++ src/Components/Hero.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <>
+    <h1 className='m-5 fs-1'>Welcome to Landing page</h1>
+    </>
+  )
+}
+
+export default Hero
